fcba74f9cdcf57adcce9cdb28913857116e4d144
Dasha06 <dasha.nikitina20@yandex.ru>
2024-11-27 19:05:12 +0300

    добавлена команда для показа спама и изменена проверка сообщения на спам, СЛЕДИ ЧТО ПИШЕШЬ)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import logging
 import re
+from asyncio import sleep  # Добавьте этот импорт
 
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
@@ -31,40 +32,54 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+    await update.message.reply_text("AddSpam <сообщение> - добавление шаблона спама\n ShowSpam - показать существующие шаблоны")
 
 async def add_spam_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     with open('spam.txt', 'a') as file:
         file.write("\n"+update.message.text[9:])
-    await update.message.reply_text("Шаблон спама добавлен.")
+    await update.message.delete()
+    message = await update.message.reply_text("Шаблон спама добавлен.")
+    await sleep(5)
+    await message.delete()
 
+async def show_spam_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    with open('spam.txt', 'r') as file:
+        spam_list = file.read().splitlines()
+        await update.message.reply_text('\n'.join(spam_list))
 
 async def delete_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.message.text.startswith('/ShowSpam'):
+        return  # Если сообщение начинается с команды ShowSpam, выходим из функции
     with open('spam.txt', 'r') as file:
-        spam_list = file.read().splitlines()  # Читаем файл и разбиваем на строки
+        spam_list = file.read().splitlines()
         for spam in spam_list:
-            # Создаем регулярное выражение для проверки с учетом лишних слов
-            pattern = re.sub(r'\\s+', r'\\s*', re.escape(spam))  # Заменяем пробелы на пробелы с возможными лишними словами
-            if re.search(pattern, update.message.text, re.IGNORECASE):  # Проверяем, есть ли спам в сообщении
+            words = spam.split()
+            pattern = '|'.join([re.escape(word) for word in words])  # Создаем шаблон из отдельных слов, игнорируя регистр
+            if all(re.search(word, update.message.text, re.IGNORECASE) for word in words):
                 await update.message.delete()
-                chat_id = update.message.chat.id  # Исправлено: chat_id
+                chat_id = update.message.chat.id
                 user_id = update.message.from_user.id if update.message.from_user else None
-                await context.bot.ban_chat_member(chat_id, user_id)
-                break  # Выходим из цикла после нахождения спама
+                try:
+                    await context.bot.ban_chat_member(chat_id, user_id)
+                except Exception as e:
+                    print(f"Ошибка: {e}")
+                print('забан')
+                break
 
 
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("<YOUR-TOKEN>").build()
+    application = Application.builder().token("").build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("AddSpam", add_spam_command))
+    application.add_handler(CommandHandler("ShowSpam", show_spam_command))
 
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT, delete_message))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, delete_message))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
